3adf40ca7ca03a50040785d8edafa33f9cf0c366
Evon Binns <evon@webbabox.com>
2018-11-09 23:39:14 -0500

    Crud api complete

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,18 +4,47 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 // Get Posts
-
-router.get('/', (req, res) => {
-  res.send('hello');
+router.get('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
 });
 
 // Add Post
-
+router.post('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createAt: new Date()
+  });
+  res.status(201).send();
+});
 
 // Update Post
-
+router.put('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.findOneAndUpdate(
+    {_id: new mongodb.ObjectID(req.body.id)},
+    {$set: {text: req.body.text, updatedAt: new Date()}}
+  );
+  res.status(200).send();
+});
 
 // Delete Post
+router.delete('/:id', async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+  res.status(200).send();
+});
 
+// Connect to Post Collection
+
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect
+  ('mongodb://processTest:process1234@ds157493.mlab.com:57493/vue_process', {
+    useNewUrlParser: true
+  });
+
+  return client.db('vue_process').collection('posts');
+}
 
 module.exports = router;
