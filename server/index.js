0cba9299ac71302a8e92deb90e47a8097b942c1e
Evon Binns <evon@webbabox.com>
2018-11-09 22:09:46 -0500

    express setup cont'd

--- server/index.js
+++ server/index.js
@@ -5,8 +5,12 @@ const cors = require('cors');
 const app = express();
 
 // Middleware
-app,use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(cors());
+
+const posts = require('./routes/api/posts');
+
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
---
+++ server/routes/api/posts.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get Posts
+
+router.get('/', (req, res) => {
+  res.send('hello');
+});
+
+// Add Post
+
+
+// Update Post
+
+
+// Delete Post
+
+
+module.exports = router;
