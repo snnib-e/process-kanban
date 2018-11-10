2e516fc04f58926e34fd0a0ec523c7d7b6cadb3d
Evon Binns <evon@webbabox.com>
2018-11-09 21:52:43 -0500

    Update server/index.js

--- server/index.js
+++ server/index.js
@@ -10,4 +10,4 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log('Server started on port ${port}'));
+app.listen(port, () => console.log(`Server started on port ${port}`));
